45bef3615a4aed0dc49bd04bf5d93e2d2920c514
JaxtonHale <theslammer100@gmail.com>
2017-04-04 18:02:32 -0400

    random changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -30,3 +30,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Inconsolata" :foundry "PfEd" :slant normal :weight normal :height 113 :width normal)))))
+
+(cua-mode)
